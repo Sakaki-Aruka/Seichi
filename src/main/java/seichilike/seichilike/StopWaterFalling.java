4c78ee8e224dc569e8978c2236e4fc4a8f7e90bf
Sakaki-Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2022-08-21 08:45:21 +0900

    update : stop falling water

--- src/main/java/seichilike/seichilike/StopWaterFalling.java
+++ src/main/java/seichilike/seichilike/StopWaterFalling.java
@@ -1,9 +1,11 @@
 package seichilike.seichilike;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFromToEvent;
 
 public class StopWaterFalling implements Listener {
+    @EventHandler
     public void onBlockFromTo(BlockFromToEvent event){
         if(event.getBlock().getType().name().equalsIgnoreCase("WATER")){
             event.setCancelled(true);
