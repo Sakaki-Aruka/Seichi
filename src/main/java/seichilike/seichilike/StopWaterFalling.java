09e1cebdd7af38c3a056a5ca7970731d651936d8
Sakaki-Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2022-08-21 09:02:02 +0900

    update : water falling

--- src/main/java/seichilike/seichilike/StopWaterFalling.java
+++ src/main/java/seichilike/seichilike/StopWaterFalling.java
@@ -1,5 +1,6 @@
 package seichilike.seichilike;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFromToEvent;
@@ -9,7 +10,7 @@ public class StopWaterFalling implements Listener {
     public void onBlockFromTo(BlockFromToEvent event){
         System.out.println("ToBlock:"+event.getToBlock().getType().name());
         System.out.println("Block:"+event.getBlock().getType().name());
-        if(event.getBlock().getType().name().equals("WATER")){
+        if(event.getBlock().getType() == Material.WATER){
             //water falling event
             event.setCancelled(true);
             System.out.println("Event cancelled.");
